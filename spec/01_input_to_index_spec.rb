b0ac6dc279969c79ab19ccd3294547dd5918647c
Amanda Ackerman <ackerm44@gmail.com>
2017-10-16 16:06:14 +0000

    automatically backed up by learn

--- spec/01_input_to_index_spec.rb
+++ spec/01_input_to_index_spec.rb
@@ -3,7 +3,7 @@ require_relative "../lib/move.rb"
 describe '#input_to_index' do
 
   it 'converts a user_input to an integer' do
-    user_input = "3"
+    user_input = "1"
 
     expect(input_to_index(user_input)).to be_a(Integer)
   end
